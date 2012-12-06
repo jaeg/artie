1dd1f8aba92f7d90e8c0e5ec19f65a3a513a04c5
John <John@localhost>
2012-12-05 21:15:16 -0500

    Added question and statement learning.

--- src/artie/ai/Artie.java
+++ src/artie/ai/Artie.java
@@ -1,6 +1,8 @@
 package artie.ai;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 import artie.database.Database;
@@ -20,6 +22,7 @@ public class Artie
 	private double trainingImpact; // Loaded from settings file
 	private Database statementDatabase;
 	private Database questionDatabase;
+	private String newMessage;
 
 	public Artie()
 	{
@@ -124,34 +127,36 @@ public class Artie
 
 	private String handleInput(Database database, SentenceType sentenceType)
 	{
-		String possibleResponse ="";
-		if (questionLearning == LearningStage.NOT_ENGAGED && statementLearning == LearningStage.NOT_ENGAGED)
+		String possibleResponse = "";
+		if (questionLearning == LearningStage.NOT_ENGAGED
+				&& statementLearning == LearningStage.NOT_ENGAGED)
 		{
 			String[] keywords = tokenizeSentence();
 			possibleResponse = database.getResponse(keywords);
 
 			if (possibleResponse == null)
 			{
-				possibleResponse = learn();
+				return learn();
 			}
 
 			if (possibleResponse.equals(lastResponse))
 			{
-				if (database.getSecondBestResponseWeight() > 0.60 && database.getSecondBestResponseMessage()!=null)
+				if (database.getSecondBestResponseWeight() > 0.60
+						&& database.getSecondBestResponseMessage() != null)
 					possibleResponse = database.getSecondBestResponseMessage();
 				else
-					possibleResponse = learnStatement();
+					return learn();
 			}
 			else if (database.getResponseWeight() < 0.60)
 			{
-				possibleResponse = learn();
+				return learn();
 			}
 		}
 		else
 		{
-			possibleResponse = learn();
+			return learn();
 		}
-		
+
 		if (possibleResponse.contains("[+]"))
 		{
 			train(trainingImpact);
@@ -177,25 +182,102 @@ public class Artie
 
 	private void train(double amount)
 	{
-		Logger.Log("Training engaged.  Effect on keywords should be: "+amount);
+		Logger.Log("Training engaged.  Effect on keywords should be: " + amount);
 	}
 
 	private String learn()
 	{
+		if (statementLearning != LearningStage.NOT_ENGAGED)
+			return learnStatement();
+		if (questionLearning != LearningStage.NOT_ENGAGED)
+			return learnQuestion();
+		
 		if (sentenceType == SentenceType.STATEMENT)
 			return learnStatement();
 		else
 			return learnQuestion();
 	}
-	
+
 	private String learnStatement()
 	{
-		return "Learning Statement Engaged";
+		LinkedList<String> keywords = new LinkedList<String>(
+				Arrays.asList(tokenizeSentence()));
+		if (statementLearning == LearningStage.NOT_ENGAGED)
+		{
+			statementLearning = LearningStage.ASK_ANSWER;
+			return "I don't know much about that.  Can you tell me more?";
+		}
+
+		else if (statementLearning == LearningStage.ASK_ANSWER)
+		{
+			newMessage = userInput;
+			statementLearning = LearningStage.ASK_IF_TRUE;
+			return "Is that true?";
+		}
+
+		else if (statementLearning == LearningStage.ASK_IF_TRUE)
+		{
+
+			if (keywords.contains("YES") || keywords.contains("YAH")
+					|| keywords.contains("YEP") || keywords.contains("SURE"))
+			{
+				try
+				{
+					statementDatabase.writeResponse(
+							statementDatabase.getLastKeywordsGiven(),
+							newMessage);
+				}
+				catch (Exception e)
+				{
+					return "I failed to remember that...";
+				}
+				statementLearning = LearningStage.NOT_ENGAGED;
+				return "That's for informing me!";
+			}
+		}
+		return "Maybe some other time then";
+
 	}
 
 	private String learnQuestion()
 	{
-		return "Learning Question Engaged";
+		LinkedList<String> keywords = new LinkedList<String>(
+				Arrays.asList(tokenizeSentence()));
+		if (questionLearning == LearningStage.NOT_ENGAGED)
+		{
+			Logger.Log("Test");
+			questionLearning = LearningStage.ASK_ANSWER;
+			return "I don't have an answer for that, can you tell me?";
+		}
+
+		else if (questionLearning == LearningStage.ASK_ANSWER)
+		{
+			newMessage = userInput;
+			questionLearning = LearningStage.ASK_IF_TRUE;
+			return "Is that true?";
+		}
+
+		else if (questionLearning == LearningStage.ASK_IF_TRUE)
+		{
+
+			if (keywords.contains("YES") || keywords.contains("YAH")
+					|| keywords.contains("YEP") || keywords.contains("SURE"))
+			{
+				try
+				{
+					questionDatabase.writeResponse(
+							questionDatabase.getLastKeywordsGiven(),
+							newMessage);
+				}
+				catch (Exception e)
+				{
+					return "I failed to remember that...";
+				}
+				questionLearning = LearningStage.NOT_ENGAGED;
+				return "That's one for the books.";
+			}
+		}
+		return "Maybe some other time then";
 	}
 
 }
--- src/artie/database/Database.java
+++ src/artie/database/Database.java
@@ -1,13 +1,21 @@
 package artie.database;
 
 import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Random;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
@@ -25,6 +33,7 @@ public class Database
 	private Node secondBestResponse; // If there is a repeat use this one
 										// instead.
 	private LinkedList<Node> lastKeywords;
+	private LinkedList<String> lastKeywordsGiven;
 	private double responseWeight;
 	private double secondBestWeight;
 
@@ -48,6 +57,8 @@ public class Database
 	public String getResponse(String[] keywords)
 	{
 		lastResponse = currentResponse;
+		lastKeywordsGiven = new LinkedList<String>(
+				Arrays.asList(keywords));
 		getResponseWithHeighestWeight(keywords);
 		return getResponseMessage();
 	}
@@ -60,8 +71,8 @@ public class Database
 	// TODO Get a random message from a response
 	public String getResponseMessage()
 	{
-		Element responseElement = (Element)currentResponse;
-		if (responseElement!=null)
+		Element responseElement = (Element) currentResponse;
+		if (responseElement != null)
 		{
 			NodeList messages = responseElement.getElementsByTagName("Message");
 			Random random = new Random();
@@ -76,8 +87,8 @@ public class Database
 	public String getSecondBestResponseMessage()
 	{
 		Logger.Log("Get second best response.");
-		Element responseElement = (Element)secondBestResponse;
-		if (responseElement!=null)
+		Element responseElement = (Element) secondBestResponse;
+		if (responseElement != null)
 		{
 			NodeList messages = responseElement.getElementsByTagName("Message");
 			Random random = new Random();
@@ -99,6 +110,11 @@ public class Database
 		return lastKeywords;
 	}
 
+	public LinkedList<String> getLastKeywordsGiven()
+	{
+		return lastKeywordsGiven;
+	}
+	
 	public double getResponseWeight()
 	{
 		return responseWeight;
@@ -110,8 +126,51 @@ public class Database
 	}
 
 	// TODO create a new response from scratch
-	public void writeResponse(String[] keywords, String[] message)
+	public void writeResponse(LinkedList<String> keywords, String message)
+			throws Exception
 	{
+
+		Node root = doc.getFirstChild();
+
+		Node responseNode = doc.createElement("Response");
+		Node keywordsNode = doc.createElement("Keywords");
+		Node messagesNode = doc.createElement("Messages");
+		for (String keyword : keywords)
+		{
+			Node keywordNode = doc.createElement("Keyword");
+			keywordNode.setTextContent(keyword);
+			Attr weight = doc.createAttribute("weight");
+			Random generator = new Random();
+			double weightValue = 0.4 + (0.6 - 0.4) * generator.nextDouble();
+			weight.setValue(Double.toString(weightValue));
+			keywordNode.getAttributes().setNamedItem(weight);
+			keywordsNode.appendChild(keywordNode);
+		}
+
+		Node messageNode = doc.createElement("Message");
+		messageNode.setTextContent(message);
+		messagesNode.appendChild(messageNode);
+
+		responseNode.appendChild(keywordsNode);
+		responseNode.appendChild(messagesNode);
+
+		root.appendChild(responseNode);
+
+		Transformer transformer = TransformerFactory.newInstance()
+				.newTransformer();
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+		// initialize StreamResult with File object to save to file
+		StreamResult result = new StreamResult(new StringWriter());
+		DOMSource source = new DOMSource(doc);
+		transformer.transform(source, result);
+
+		String xmlString = result.getWriter().toString();
+
+		// File file = new File(path);
+		PrintWriter out = new PrintWriter(database);
+		out.println(xmlString);
+		out.close();
 
 	}
 
@@ -130,54 +189,60 @@ public class Database
 	// TODO Add code to get the best and second best response from database.
 	private void getResponseWithHeighestWeight(String[] keywords)
 	{
-		
+
 		LinkedList<Node> responses = new LinkedList<Node>();
 		responses = getResponsesWithSimilarKeywords(keywords);
-		getBestResponseNode(responses,keywords);
+		getBestResponseNode(responses, keywords);
 	}
-	
+
 	private LinkedList<Node> getResponsesWithSimilarKeywords(String[] keywords)
 	{
 		NodeList keywordsInDatabase = doc.getElementsByTagName("Keyword");
 		LinkedList<Node> responses = new LinkedList<Node>();
-		LinkedList<String> keywordsList = new LinkedList<String>(Arrays.asList(keywords));
-		
-		for (int i=0; i<keywordsInDatabase.getLength();i++)
+		LinkedList<String> keywordsList = new LinkedList<String>(
+				Arrays.asList(keywords));
+
+		for (int i = 0; i < keywordsInDatabase.getLength(); i++)
 		{
 			Node currentKeyword = keywordsInDatabase.item(i);
 			if (keywordsList.contains(currentKeyword.getTextContent()))
 			{
-				Node responseNode = currentKeyword.getParentNode().getParentNode();
+				Node responseNode = currentKeyword.getParentNode()
+						.getParentNode();
 				if (!responses.contains(responseNode))
 					responses.add(responseNode);
-			}			
+			}
 		}
-		
+
 		return responses;
 	}
 
-	private void getBestResponseNode(LinkedList<Node> responses,String[] keywords)
+	private void getBestResponseNode(LinkedList<Node> responses,
+			String[] keywords)
 	{
 		double best = 0.0;
 		double current = 0.0;
 		Node bestResponse = null;
-		LinkedList<String> keywordsList = new LinkedList<String>(Arrays.asList(keywords));
+		LinkedList<String> keywordsList = new LinkedList<String>(
+				Arrays.asList(keywords));
 		for (Node response : responses)
 		{
-			Element responseElement = (Element)response;
-			NodeList responseKeywordNodes = responseElement.getElementsByTagName("Keyword");
-			for (int i=0; i<responseKeywordNodes.getLength();i++)
+			Element responseElement = (Element) response;
+			NodeList responseKeywordNodes = responseElement
+					.getElementsByTagName("Keyword");
+			for (int i = 0; i < responseKeywordNodes.getLength(); i++)
 			{
 				Node currentKeyword = responseKeywordNodes.item(i);
 				if (keywordsList.contains(currentKeyword.getTextContent()))
 				{
 					NamedNodeMap attributes = currentKeyword.getAttributes();
-					double weight = Double.parseDouble(attributes.getNamedItem("weight").getNodeValue());
+					double weight = Double.parseDouble(attributes.getNamedItem(
+							"weight").getNodeValue());
 					current += weight;
 				}
 			}
-			
-			if (current>best)
+
+			if (current > best)
 			{
 				secondBestResponse = bestResponse;
 				secondBestWeight = best;
@@ -187,7 +252,7 @@ public class Database
 				responseWeight = best;
 				currentResponse = response;
 			}
-			
+
 		}
 	}
 }
